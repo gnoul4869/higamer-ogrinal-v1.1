c97340274263e80b2bd8e6a3b1172b672895b71a
gnoul_ <ttluong4869@gmail.com>
2022-04-20 22:28:23 +0700

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,21 +1,10 @@
 <?php
-// XAMPP MySQL
-define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'QuanLyDatHang');
 
-// 123HOST MySQL
-// define('HOST', '103.97.125.245');
-// define('USERNAME', 'megafunx_megafunx');
-// define('PASSWORD', '$SZ{sXFYBp8d');
-// define('DATABASE', 'megafunx_higamer');
-
-// AwardSpace MySQL
-// define('HOST', 'fdb18.awardspace.net');
-// define('USERNAME', '3832584_higamer');
-// define('PASSWORD', '^y{at3K^8(,HTLq9');
-// define('DATABASE', '3832584_higamer');
+123HOST MySQL
+define('HOST', '103.97.125.245');
+define('USERNAME', 'megafunx_megafunx');
+define('PASSWORD', '$SZ{sXFYBp8d');
+define('DATABASE', 'megafunx_higamer');
 
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
